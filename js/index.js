36f9b8b313a2268790589bbed5cf8e0483f3da75
Sebastian Lorensson <34066570+SebLore@users.noreply.github.com>
2025-03-08 22:51:04 +0100

    fold navbar

--- js/index.js
+++ js/index.js
@@ -9,16 +9,24 @@ function toggleMenu () {
     }
 }
 
+/**
+ * Toggle the visibility of the navbar when the user scrolls up or down
+ * and move the page content up or down accordingly.
+ */
 let lastScrollY = window.scrollY;
 const navbar = document.querySelector(".navbar");
+const page = document.querySelector(".page");
 
 window.addEventListener("scroll", () => {
     if (window.scrollY > lastScrollY) {
         // Scrolling down
         navbar.style.transform = "translateY(-100%)";
+        const offset = navbar.offsetHeight;
+        // page.style.transform = `translateY(-${offset}px)`;
     } else {
         // Scrolling up
         navbar.style.transform = "translateY(0)";
+        // page.style.transform = "translateY(0)";
     }
     lastScrollY = window.scrollY;
 });
