b13832a68b6d4da81e8745d9e4111395eab20aa6
Sebastian Lorensson <34066570+SebLore@users.noreply.github.com>
2025-03-08 22:40:44 +0100

    more phone adaptation

--- js/index.js
+++ js/index.js
@@ -8,3 +8,17 @@ function toggleMenu () {
         navbarContent.style.display = "none";
     }
 }
+
+let lastScrollY = window.scrollY;
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > lastScrollY) {
+        // Scrolling down
+        navbar.style.transform = "translateY(-100%)";
+    } else {
+        // Scrolling up
+        navbar.style.transform = "translateY(0)";
+    }
+    lastScrollY = window.scrollY;
+});
